2c188d7e73c948395fc21599e15a7f36e53a2093
Alex Zidros <alexander.zidros@gmail.com>
2021-03-09 16:30:15 -0500

    Create core.ino

---
+++ core/core.ino
@@ -0,0 +1,21 @@
+#include <Arduino.h>
+#include <EEPROM.h>
+#include <Keyboard.h>
+
+void setup() {
+  delay(1000);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
+
+void get_eeprom_keys(){
+  
+}
+
+void keyToPinMapper(){
+  
+}
